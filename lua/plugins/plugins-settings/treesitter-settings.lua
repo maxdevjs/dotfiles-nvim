61e982c6b0f8117b6ee05a98008cd9fd8b4dea32
maxdevjs <22229196+maxdevjs@users.noreply.github.com>
2022-09-25 23:13:14 -0300

    Fix nvim-treesitter.configs ensure_installed

--- lua/plugins/plugins-settings/treesitter-settings.lua
+++ lua/plugins/plugins-settings/treesitter-settings.lua
@@ -1,6 +1,16 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    ignore_install = { "javascript" }, -- List of parsers to ignore installing
+    -- https://github.com/nvim-treesitter/nvim-treesitter/issues/2887#issuecomment-1126787909
+    -- https://github.com/nvim-treesitter/nvim-treesitter/issues/2886#issuecomment-1120015543
+    -- https://github.com/ogirginc/dotfiles/commit/3c85abd07227f9bab4398809668784e1aa03e1b1
+    -- ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    
+    -- https://tree-sitter.github.io/tree-sitter/creating-parsers#installation
+    -- https://crates.io/crates/tree-sitter-cli
+    -- $ npm install -g tree-sitter-cli -- ex.: for gitignore
+    
+    -- https://github.com/nvim-treesitter/nvim-treesitter
+    ensure_installed = {'bash', 'comment', 'css',  'fish', 'gitignore', 'html', 'http', 'javascript', 'jsdoc', 'json', 'lua', 'markdown', 'nix', 'prisma', 'python', 'regex', 'ruby', 'rust', 'scss', 'sql', 'svelte', 'sxhkdrc', 'tsx', 'typescript', 'vue', 'yaml'},
+    --ignore_install = { "javascript" }, -- List of parsers to ignore installing
     highlight = {
       enable = true,              -- false will disable the whole extension
       disable = { "c", "rust" },  -- list of language that will be disabled
