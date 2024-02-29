9ebdf723d0315f3ae78dccca56cd5440dfbe40bb
maxdevjs <22229196+maxdevjs@users.noreply.github.com>
2024-02-29 11:58:50 +0100

    Add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,8 +4,30 @@
 local Util = require("lazyvim.util")
 local map = Util.safe_keymap_set
 
--- Control + a to select all file
+-- TODO: enters command mode, but does not automatically show the UI (as instead does :)
+map("n", ";", ":", { desc = "; to :"})
 map("n", "<C-a>", "<<Esc>ggVG", { desc = "Control + a to select all file"})
--- Open the current file in the default program
 map("n", "<C-x>", ":!xdg-open %<CR>", { desc = "Open the current file in the default program"})
+
+-- Buffers
+map("n", "<TAB>", ":bnext<CR>", { desc = "Switch to next buffer"})
+map("n", "<S-TAB>", ":bprevious<CR>", { desc = "Switch to previous buffer"})
+map("x", "<leader>p", "\"_dP", { desc = "Substitute current visual block without loosing current paste buffer"})
+
+-- Markdown nicities
+map("i", "-[", "- [ ] []()", { desc = "- [ ] []()"})
+
+-- Undo break points ⛏️
+map("i", ".", ".<C-G>u", { desc = "Set break point"})
+map("i", ",", ",<C-G>u", { desc = "Set break point"})
+map("i", ":", ":<C-G>u", { desc = "Set break point"})
+map("i", ";", ";<C-G>u", { desc = "Set break point"})
+map("i", "!", "!<C-G>u", { desc = "Set break point"})
+map("i", "?", "?<C-G>u", { desc = "Set break point"})
+map("i", "(", "(<C-G>u", { desc = "Set break point"})
+map("i", ")", ")<C-G>u", { desc = "Set break point"})
+map("i", "[", "[<C-G>u", { desc = "Set break point"})
+map("i", "]", "]<C-G>u", { desc = "Set break point"})
+map("i", "{", "{<C-G>u", { desc = "Set break point"})
+map("i", "}", "}<C-G>u", { desc = "Set break point"})
 
