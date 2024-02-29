bb81e9b80071adee18449057f5ac8bb9cdfca8ba
maxdevjs <22229196+maxdevjs@users.noreply.github.com>
2024-02-29 02:31:23 +0100

    Add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local Util = require("lazyvim.util")
+local map = Util.safe_keymap_set
+
+-- Control + a to select all file
+map("n", "<C-a>", "<<Esc>ggVG", { desc = "Control + a to select all file"})
+-- Open the current file in the default program
+map("n", "<C-x>", ":!xdg-open %<CR>", { desc = "Open the current file in the default program"})
+
