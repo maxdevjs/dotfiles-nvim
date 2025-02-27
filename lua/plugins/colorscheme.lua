c4799c2d99b0e643c327c48323bea9e9db17ebd1
maxdevjs <22229196+maxdevjs@users.noreply.github.com>
2025-02-26 22:53:38 -0300

    Add e-ink.nvim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -40,6 +40,22 @@ return {
   { "ryanpcmcquen/true-monochrome_vim", enabled = false },
   { "projekt0n/github-nvim-theme", enabled = false },
   { "embark-theme/vim", enabled = false },
+  {
+    "alexxGmZ/e-ink.nvim",
+    priority = 1000,
+    config = function()
+      require("e-ink").setup()
+      vim.cmd.colorscheme("e-ink")
+
+      -- choose light mode or dark mode
+      -- vim.opt.background = "dark"
+      -- vim.opt.background = "light"
+      --
+      -- or do
+      -- :set background=dark
+      -- :set background=light
+    end,
+  },
 
   -- Configure LazyVim to load gruvbox
   {
