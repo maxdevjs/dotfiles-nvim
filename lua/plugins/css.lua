57fd7b49e3a774c730b5c0e4481e9b022beface8
maxdevjs <22229196+maxdevjs@users.noreply.github.com>
2025-06-02 14:38:12 -0300

    Setup css and tailwind lspconfig

--- lua/plugins/css.lua
+++ lua/plugins/css.lua
@@ -1,6 +1,7 @@
 -- https://github.com/brenoprata10/nvim-highlight-colors
 return {
   "brenoprata10/nvim-highlight-colors",
+  event = "VeryLazy",
   config = function()
     require("nvim-highlight-colors").setup({
       ---Render style
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,1 +1,19 @@
-return {}
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      cssls = {
+        settings = {
+          -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#cssls
+          -- https://www.reddit.com/r/lunarvim/comments/w50jfk/comment/jcl262v/
+          css = {
+            validate = true,
+            lint = {
+              unknownAtRules = "ignore",
+            },
+          },
+        },
+      },
+    },
+  },
+}
