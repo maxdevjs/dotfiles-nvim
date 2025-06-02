d9e2acd7ce5ca3b09a2a527443bf7bf7c3da4480
maxdevjs <22229196+maxdevjs@users.noreply.github.com>
2025-06-02 14:40:51 -0300

    Temporary (?) fix for who knows what is broken

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,1 +1,6 @@
-return {}
+-- https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2859309755
+-- return {}
+return {
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
